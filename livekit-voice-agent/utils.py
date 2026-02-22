4c4eed928a542acbef5af3267f838098dd530ed3
Kamal-Moha <Kamalmohapy@gmail.com>
2026-02-22 21:39:54 +0300

    Find the current directory so as to get the path to prompts folder

--- livekit-voice-agent/utils.py
+++ livekit-voice-agent/utils.py
@@ -4,6 +4,7 @@ import yaml
 def load_prompt(filename):
   """Load a prompt from a YAML file."""
   script_dir = os.getcwd()
+  print(f"PATH DIRECTORY: {script_dir}")
   # prompt_path = os.path.join(script_dir, filename)
   prompt_path = os.path.join(f"/prompts/{filename}")
 
