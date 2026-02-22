f95cbaedb2b3f9eade062526a2e24e4656dc31fc
Kamal-Moha <Kamalmohapy@gmail.com>
2026-02-23 01:26:56 +0300

    Fixed the prompts path issue

--- livekit-voice-agent/utils.py
+++ livekit-voice-agent/utils.py
@@ -2,16 +2,16 @@ import os
 import yaml
 
 def load_prompt(filename):
-  """Load a prompt from a YAML file."""
-  script_dir = os.getcwd()
-  print(f"LIST DIR: {os.listdir()}")
-  # prompt_path = os.path.join(script_dir, filename)
-  prompt_path = os.path.join(f"/livekit-voice-agent/prompts/{filename}")
+    """Load a prompt from a YAML file."""
+    script_dir = os.getcwd()
+    # print(f"LIST DIR: {os.listdir()}")
+    # prompt_path = os.path.join(f"/livekit-voice-agent/prompts/{filename}")
+    prompt_path = os.path.join(script_dir, 'prompts', filename)
 
-  try:
-      with open(prompt_path, 'r') as file:
-          prompt_data = yaml.safe_load(file)
-          return prompt_data.get('instructions', '')
-  except (FileNotFoundError, yaml.YAMLError) as e:
-      print(f"Error loading prompt file {filename}: {e}")
-      return ""
+    try:
+        with open(prompt_path, 'r') as file:
+            prompt_data = yaml.safe_load(file)
+            return prompt_data.get('instructions', '')
+    except (FileNotFoundError, yaml.YAMLError) as e:
+        print(f"Error loading prompt file {filename}: {e}")
+        return ""
